a67b803d713e30b006a6be8bf69f10d3052c2073
CebotaruDaniel <daniel.cebotaru@iis.utm.md>
2023-02-10 23:35:38 +0200

    v1.2

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 AOS.init();
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".button", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1.5");
+tl.to(".text", { y: "-50%", duration: 1, stagger: 0.25 }, "-=1.5");
