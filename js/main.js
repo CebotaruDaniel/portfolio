b99d1845ca3f5934205220ed5b4745b5204ddcf7
CebotaruDaniel <daniel.cebotaru@iis.utm.md>
2023-02-13 11:23:35 +0200

    v1.4

--- js/main.js
+++ js/main.js
@@ -6,3 +6,4 @@ tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".button", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1.5");
 tl.to(".text", { y: "-50%", duration: 1, stagger: 0.25 }, "-=1.5");
+
