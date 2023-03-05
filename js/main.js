ef1ac50ede94f6ce1881a28188d26d55ab5f1c39
CebotaruDaniel <daniel.cebotaru@iis.utm.md>
2023-03-05 14:09:26 +0200

    v1.4.5

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ AOS.init();
 
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".button", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1.5");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1.5 });
+tl.fromTo(".button", { opacity: 0 }, { opacity: 1, duration: 1.5 });
+tl.fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=1.5");
 tl.to(".text", { y: "-50%", duration: 1, stagger: 0.25 }, "-=1.5");
 
